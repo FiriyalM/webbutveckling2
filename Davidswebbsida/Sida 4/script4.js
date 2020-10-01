e612b05c472068a2405e1c5004ea6f1589fabf8b
FiriyalM <43432879+FiriyalM@users.noreply.github.com>
2020-10-01 14:16:28 +0200

    alla 4 sidor nästan klara

---
+++ Davidswebbsida/Sida 4/script4.js
@@ -0,0 +1,23 @@
+let pages = ["Sida 1", "Sida 2", "Sida 3", "Sida 4"];
+let ul;
+
+window.onload = init;
+
+function init(){
+    ul = document.getElementById("Nav");  
+
+    for(let i = 0; i < pages.length; i++){
+        createNavElement(pages[i], i);
+    }
+}
+
+function createNavElement(name, index){
+    let li = document.createElement("li");
+    li.setAttribute("id", "ulli" + index);
+    ul.appendChild(li);
+
+    let a = document.createElement("a");
+    a.setAttribute("href", "../Sida"+ (index+1) +"/Sida.html");
+    a.innerHTML = name;
+    document.getElementById("ulli" + index).appendChild(a);
+}
